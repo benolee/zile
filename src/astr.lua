67663fb677bc6022fa2d70ed0892b279580ce285
Reuben Thomas <rrt@sc3d.org>
2012-10-01 22:44:23 +0100

    build: require alien 0.6.1 (not 0.7.0 for now)

--- src/astr.lua
+++ src/astr.lua
@@ -17,8 +17,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require "alien"
-
 local allocation_chunk_size = 16
 AStr = Object {
   _init = function (self, s)
